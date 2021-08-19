b8dc8d04d939becbf89a729ed9c1e81b1bf75999
nedillon <78570202+nedillon@users.noreply.github.com>
2021-08-19 16:51:53 -0500

    Added model for Add Friends page

---
+++ UI/Models/Members/AddFriends.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models.Members
+{
+    /// <summary>
+    /// Model used by the Add Friends page
+    /// </summary>
+    public class AddFriends
+    {
+
+        /// <summary>
+        /// Primary member ID
+        /// </summary>
+        [Required(ErrorMessage = "Primary friend is required", AllowEmptyStrings = false)]
+        public long PrimaryID { get; set; }
+
+        /// <summary>
+        /// Secondary member ID
+        /// </summary>
+        [Required(ErrorMessage = "Secondary friend is required", AllowEmptyStrings = false)]
+        public long SecondaryID { get; set; }
+
+        /// <summary>
+        /// Success message (if any)
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Error message (if any)
+        /// </summary>
+        public string ErrorMessage { get; set; }
+
+    }
+}
