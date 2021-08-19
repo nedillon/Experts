3e3436baf25513f2bb6a471bdd34f2548179d10b
nedillon <78570202+nedillon@users.noreply.github.com>
2021-08-19 14:53:31 -0500

    Added View Member model

---
+++ UI/Models/Members/MemberFriend.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models.Members
+{
+    /// <summary>
+    /// Model used by the View Member page to list friends of the member
+    /// </summary>
+    public class MemberFriend
+    {
+
+        /// <summary>
+        /// Member ID of the friend
+        /// </summary>
+        public long ID { get; set; }
+
+        /// <summary>
+        /// Name of the friend
+        /// </summary>
+        public string Name { get; set; }
+
+    }
+}
---
+++ UI/Models/Members/MemberView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models.Members
+{
+    /// <summary>
+    /// Model used by the View Member page
+    /// </summary>
+    public class MemberView
+    {
+
+        /// <summary>
+        /// Member ID
+        /// </summary>
+        public long ID { get; set; }
+        
+        /// <summary>
+        /// Member Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Original (long) website URL
+        /// </summary>
+        public string Website { get; set; }
+
+        /// <summary>
+        /// Shortened website URL
+        /// </summary>
+        public string ShortURL { get; set; }
+
+        /// <summary>
+        /// List of headings (areas of expertise) for the member
+        /// </summary>
+        public List<string> Headings { get; set; }
+
+        /// <summary>
+        /// List of friends of the member
+        /// </summary>
+        public List<MemberFriend> Friends { get; set; }
+
+    }
+}
