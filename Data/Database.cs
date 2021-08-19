4e6e3e8f2ede6bb1a20634efe3b782a4316b15db
nedillon <78570202+nedillon@users.noreply.github.com>
2021-08-19 14:44:43 -0500

    Added some default data

--- Data/Database.cs
+++ Data/Database.cs
@@ -15,6 +15,12 @@ namespace Data
     class Database
     {
 
+        static Database()
+        {
+            //When the application starts up, insert some dummy data
+            CreateSomeTestData();
+        }
+
         //Our "database". A DataSet that is created as a Singleton so that there is only ever one instance
         private static ExpertData _db = new ExpertData();
 
@@ -22,5 +28,39 @@ namespace Data
         /// The "database"
         /// </summary>
         public static ExpertData db { get { return _db; } }
+
+        private static void CreateSomeTestData()
+        {
+            //Clear out any old data (just in case)
+            db.Friendships.Clear();
+            db.MemberHeadings.Clear();
+            db.Members.Clear();
+
+            //Add 4 members
+            db.Members.AddMembersRow("Nick Dillon", "http://LongerURL/That/Has/More/Stuff", "http://blah.com/short");
+            db.Members.AddMembersRow("Bob Bobson", "http://LongerURL/That/Has/Even/More/Stuff", "http://blah.com/a");
+            db.Members.AddMembersRow("Jane Janedatter", "http://LongerURL/That/Has/Even/More/Stuff/Still", "http://blah.com/b");
+            db.Members.AddMembersRow("Tom Thompson", "http://LongerURL/Somewhat", "http://blah.com/shortc");
+
+            //Give each member some headings (areas of expertise)
+            db.MemberHeadings.AddMemberHeadingsRow(db.Members[0], "One");
+            db.MemberHeadings.AddMemberHeadingsRow(db.Members[0], "Two");
+            db.MemberHeadings.AddMemberHeadingsRow(db.Members[0], "Three");
+
+            db.MemberHeadings.AddMemberHeadingsRow(db.Members[1], "Some stuff");
+
+            db.MemberHeadings.AddMemberHeadingsRow(db.Members[2], "Look at me");
+            db.MemberHeadings.AddMemberHeadingsRow(db.Members[2], "I know that");
+
+            db.MemberHeadings.AddMemberHeadingsRow(db.Members[3], "What now?");
+            db.MemberHeadings.AddMemberHeadingsRow(db.Members[3], "Hello there");
+
+            //Create some friendships
+            db.Friendships.AddFriendshipsRow(db.Members[0], db.Members[1]);
+            db.Friendships.AddFriendshipsRow(db.Members[0], db.Members[2]);
+            db.Friendships.AddFriendshipsRow(db.Members[0], db.Members[3]);
+            db.Friendships.AddFriendshipsRow(db.Members[1], db.Members[2]);
+            db.Friendships.AddFriendshipsRow(db.Members[2], db.Members[3]);
+        }
     }
 }
