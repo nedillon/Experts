661fa7e7bd5673c8d8b17eccdab52eca47fd4ba4
nedillon <78570202+nedillon@users.noreply.github.com>
2021-08-19 15:56:16 -0500

    Added unit test project for Shared, Added unit test for Shorten URL function

---
+++ Shared.Test/GeneralOperations.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Shared.Test
+{
+    /// <summary>
+    /// Unit tests on the General operations functions
+    /// </summary>
+    [TestClass]
+    public class GeneralOperations
+    {
+        [TestMethod]
+        public void GeneralOperations_ShortenURL()
+        {
+            //Test without HTTP
+
+            string URL = "www.everlywell.com";
+
+            string ShortURL = Shared.Operations.General.ShortenURL(URL);
+
+            Assert.IsTrue(!String.IsNullOrWhiteSpace(ShortURL) && !ShortURL.Equals("www.everlywell.com"), "Not shortened without http");
+
+            //Test with HTTP
+
+            URL = "http://www.everlywell.com";
+
+            ShortURL = Shared.Operations.General.ShortenURL(URL);
+
+            Assert.IsTrue(!String.IsNullOrWhiteSpace(ShortURL) && !ShortURL.Equals("http://www.everlywell.com"), "Not shortened with http");
+
+        }
+    }
+}
---
+++ Shared.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Shared.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Shared.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b9f3da9-c154-4b5a-b608-bfb502efe436")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
