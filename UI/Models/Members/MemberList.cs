b4b80bbe6ea75bd568609dfb7ffce47fd660f2b8
nedillon <78570202+nedillon@users.noreply.github.com>
2021-08-19 14:21:48 -0500

    Added View Members model

---
+++ UI/Models/Members/MemberList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models.Members
+{
+    /// <summary>
+    /// Model used by the View Members page
+    /// </summary>
+    public class MemberList
+    {
+        /// <summary>
+        /// List of all members to be shown
+        /// </summary>
+        public List<MemberListItem> Members { get; set; }
+
+    }
+}
---
+++ UI/Models/Members/MemberListItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models.Members
+{
+    /// <summary>
+    /// Model used by the View Members page. Represents a single Member in the list
+    /// </summary>
+    public class MemberListItem
+    {
+        /// <summary>
+        /// Member ID
+        /// </summary>
+        public long ID { get; set; }
+
+        /// <summary>
+        /// Member Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Shortened URL of the member's page
+        /// </summary>
+        public string ShortURL { get; set; }
+
+        /// <summary>
+        /// Number of friends the member has
+        /// </summary>
+        public int FriendCount { get; set; }
+
+    }
+}
