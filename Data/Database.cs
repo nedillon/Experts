863e4bcf6870a6925f465fa445f493b2b537cebc
nedillon <78570202+nedillon@users.noreply.github.com>
2021-08-19 13:17:24 -0500

    Create database instance

--- Data/Database.cs
+++ Data/Database.cs
@@ -7,11 +7,20 @@ using System.Threading.Tasks;
 namespace Data
 {
     /// <summary>
-    /// Class that will serve as the connection to the database (the data layer)
+    /// Class that will serve as the connection to the database (the data layer).
+    /// 
+    /// In a production environment, this would be a class that handles querying of an ADO SQL connection or an Entity Framework instance
+    /// For testing, just using a hard-coded dataset
     /// </summary>
     class Database
     {
 
-        //TODO: Expose a database
+        //Our "database". A DataSet that is created as a Singleton so that there is only ever one instance
+        private static ExpertData _db = new ExpertData();
+
+        /// <summary>
+        /// The "database"
+        /// </summary>
+        public static ExpertData db { get { return _db; } }
     }
 }
