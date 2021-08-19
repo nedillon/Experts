4e2a084fb565a02f599a5be0f9e131d0cac48bd1
nedillon <78570202+nedillon@users.noreply.github.com>
2021-08-19 15:45:53 -0500

    Added Add Member model

---
+++ UI/Models/Members/MemberAdd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models.Members
+{
+    /// <summary>
+    /// Model used by the Add Member page
+    /// </summary>
+    public class MemberAdd
+    {
+
+        /// <summary>
+        /// Name entered for the new member
+        /// </summary>
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} is required")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Website URL
+        /// </summary>
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} is required")]
+        public string Website { get; set; }
+
+    }
+}
