365891cf0e173366d368b99d458c2e16e8e00975
nedillon <78570202+nedillon@users.noreply.github.com>
2021-08-19 14:13:09 -0500

    Added unit testing for Data project (incomplete)

---
+++ Data.Test/MemberService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace Data.Test
+{
+    /// <summary>
+    /// Unit tests on the Data.MemberService functions
+    /// </summary>
+    [TestClass]
+    public class MemberService
+    {
+        /// <summary>
+        /// Tests the AddMember function
+        /// </summary>
+        [TestMethod]
+        public void MemberService_AddMember()
+        {
+            //Get the count before
+            int count = Data.Services.MemberService.GetAllMembers().Count;
+
+            //Add a new member
+            long ID = Data.Services.MemberService.AddMember("That Guy", "http://somereallyreallyreallylongdomain.com/ThatGuy", "http://Short.url/TG", new string[] { "Heading1", "Heading2" }, null);
+
+            //Make sure an ID was returned
+            Assert.IsTrue(ID >= 0, "Member not added");
+
+            int newCount = Data.Services.MemberService.GetAllMembers().Count;
+
+            //Make sure the count increased
+            Assert.IsTrue(count + 1 == newCount, "Count not increased by one");
+
+        }
+
+        //TODO: The other service functions
+
+    }
+}
---
+++ Data.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Data.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Data.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("adc19e85-86c1-44b1-bc18-40f2f2a74367")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
