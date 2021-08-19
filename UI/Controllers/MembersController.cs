f644ab914d73c5830db892e17da8f98b383d20e3
nedillon <78570202+nedillon@users.noreply.github.com>
2021-08-19 14:24:12 -0500

    Added Members Controller skeleton

---
+++ UI/Controllers/MembersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class MembersController : Controller
+    {
+        //TODO: View Members
+
+        // GET: Members
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //TODO: View Member
+
+        //TODO: Add Member
+
+        //TODO: Add Friends
+
+        //TODO: Search for Experts
+    }
+}
