9bd01f5093cd5f5cb46c0ab611f381d0adf99f98
nedillon <78570202+nedillon@users.noreply.github.com>
2021-08-19 14:28:40 -0500

    Implemented View Members action

--- UI/Controllers/MembersController.cs
+++ UI/Controllers/MembersController.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UI.Models.Members;
+using Data.Services;
 
 namespace UI.Controllers
 {
     public class MembersController : Controller
     {
-        //TODO: View Members
 
-        // GET: Members
+        /// <summary>
+        /// Gets the view listing all membvers in the database
+        /// </summary>
+        /// <returns>The Views.Members.Index page</returns>
+        /// <remarks>GET: Members</remarks>
         public ActionResult Index()
         {
-            return View();
+            //Create the model used by the view
+            MemberList model = new MemberList();
+
+            //Load all members from the database and populate the models' list with them
+            model.Members = MemberService.GetAllMembers()
+                .Select(m => new MemberListItem() { ID = m.ID, Name = m.Name, ShortURL = m.ShortURL, FriendCount = m.Friends.Count() })
+                .ToList();
+
+            return View(model);
         }
 
         //TODO: View Member
